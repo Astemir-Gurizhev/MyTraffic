de5981e2d39537b2cf82564b7ca07e423b160c3a
Astemir-Gurizhev <gurizhev.astemir@gmail.com>
2023-11-23 20:23:04 +0300

    add script quest

---
+++ js/script.js
@@ -0,0 +1,23 @@
+let questItems = document.querySelectorAll(".quest-item")
+
+let questArrows = document.querySelectorAll(".quest-item__arrow")
+
+let questTexts = document.querySelectorAll(".quest-item__text")
+
+
+questItems.forEach(item => {
+    item.addEventListener("click", (event)=>{
+
+        console.log(event.currentTarget);
+        questArrows.forEach(item =>{
+            item.addEventListener('click', (event)=> {
+                event.target.classList.toggle("quest-item__arrow-active")
+            })
+        })
+        questTexts.forEach(item =>{
+            item.addEventListener('click', (event)=> {
+                event.target.classList.toggle("quest-item__text-active")
+            })
+        })
+    })
+})
